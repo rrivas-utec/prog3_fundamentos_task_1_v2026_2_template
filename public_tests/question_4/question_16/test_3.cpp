11fdf3072f0ada3cc87e0a3d7b096804b751acd1
Ruben Rivas <rrivas@utec.edu.pe>
2026-08-16 17:56:09 -0500

    update CMakeLists.txt

---
+++ public_tests/question_4/question_16/test_3.cpp
@@ -0,0 +1,10 @@
+
+#include "catch.hpp"
+
+// QUESTION_HEADER_PATH is resolved by CMake without distinguishing case.
+#include QUESTION_HEADER_PATH
+
+TEST_CASE("Question 16.3") {
+    // Add a small, public example for this question.
+    SUCCEED();
+}
