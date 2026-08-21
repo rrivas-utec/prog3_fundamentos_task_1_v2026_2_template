b443a386a4754a158fdc8b767afaa4794e75d4cb
Ruben Rivas <rrivas@utec.edu.pe>
2026-08-21 13:49:56 -0500

    Initial commit

--- include/p10.h
+++ include/p10.h
@@ -4,20 +4,6 @@
 
 #ifndef PROG3_FUNDAMENTOS_TASK_1_V2026_2_P10_H
 #define PROG3_FUNDAMENTOS_TASK_1_V2026_2_P10_H
-#include <cstddef>
 
-class GrayImage {
-    unsigned char* pixels_;
-    std::size_t width_;
-    std::size_t height_;
-public:
-    GrayImage(std::size_t width, std::size_t height);
-    GrayImage(GrayImage&& other) noexcept;
-    GrayImage& operator=(GrayImage&& other) noexcept;
-    GrayImage inverted() const;
-    unsigned char& at(std::size_t row, std::size_t column);
-    unsigned char at(std::size_t row, std::size_t column) const; // given
-    bool empty() const; // given
-};
 
 #endif //PROG3_FUNDAMENTOS_TASK_1_V2026_2_P10_H
